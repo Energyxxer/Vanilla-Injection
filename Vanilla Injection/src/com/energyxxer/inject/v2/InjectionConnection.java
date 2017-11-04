2d67a606b8cd5b53f14f773961ced6846bac3fad
Adrodoc55 <adrodoc55@googlemail.com>
2017-11-04 18:27:51 +0100

    Use ReadWriteLock to properly synchronize flushing and adding commands that require logging

--- Vanilla Injection/src/com/energyxxer/inject/v2/InjectionConnection.java
+++ Vanilla Injection/src/com/energyxxer/inject/v2/InjectionConnection.java
@@ -18,6 +18,8 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Consumer;
 
 import javax.annotation.Nullable;
@@ -44,12 +46,38 @@ import de.adrodoc55.minecraft.structure.Structure;
 public class InjectionConnection implements AutoCloseable {
   private final Logger logger = LogManager.getLogger();
 
+  /**
+   * The log file of the Minecraft installation. Note that when using multiple instances of
+   * Minecraft they will use the same log file and overwrite each others messages. For that reason
+   * an {@link InjectionConnection} only works properly when there is only one Minecraft instance.
+   */
   private final Path logFile;
+  /**
+   * The directory of the Minecraft world.
+   */
   private final Path worldDir;
+  /**
+   * The structure directory of the Minecraft world. This is a subdirectory of {@link #worldDir}
+   * called "structures".
+   */
   private final Path structureDir;
+  /**
+   * The unique identifier for {@code this} connection in the Minecraft world. There should never be
+   * more than one {@link InjectionConnection} with the same {@link #identifier} for each Minecraft
+   * world.
+   */
   private final String identifier;
 
+  /**
+   * The {@link Thread} to use for the periodic actions ({@link #checkLog() log checking} and
+   * {@link #flush() flushing}). The {@link #executor} is {@code null} unless the connection
+   * {@link #isOpen()}.
+   */
   private @Nullable ScheduledExecutorService executor;
+  /**
+   * The {@link LogFileReader} to use for the periodic {@link #checkLog() log checking}. The
+   * {@link #reader} is {@code null} unless the connection {@link #isOpen()}.
+   */
   private @Nullable LogFileReader reader;
 
   private @Nullable ScheduledFuture<?> logCheckFuture;
@@ -66,11 +94,46 @@ public class InjectionConnection implements AutoCloseable {
   private final Collection<SuccessListenerEntry> successListeners = new ConcurrentLinkedQueue<>();
 
   private final AtomicInteger structureId = new AtomicInteger();
-  private volatile boolean logAdminCommands;
-  private final Object logAdminCommandsLock = new Object();
+  /**
+   * This flag indicates whether or not the next flush operation should generate commands that if
+   * successful are written to the log file. This is usually set to {@code true} when listening for
+   * the output of some commands.
+   * <p>
+   * <b>Implementation Notes</b><br>
+   * This field is not {@code volatile} because it is only read while holding the exclusive write
+   * lock of {@link #logAdminCommandsLock}.
+   */
+  private boolean logAdminCommands;
+  /**
+   * This {@link ReadWriteLock} is used to prevent concurrent adding of commands that require admin
+   * command logging while flushing.<br>
+   * Adding such commands is considered to be the read operation, because multiple commands may be
+   * added concurrently.<br>
+   * Flushing is considered to be the write operation, because while flushing you may not add such a
+   * command:
+   * <ul>
+   * <li>If such a command is first added during a {@link #flush()} then it might be added to the
+   * {@link Structure} even though the preparation to enable logging has not.</li>
+   * <li>If such a command is added during a {@link #flush()} then the flag
+   * {@link #logAdminCommands} might be set to {@code false} by the {@link #flush()} operation even
+   * though the command might not have been added to the {@link Structure}.</li>
+   * </ul>
+   */
+  private final ReadWriteLock logAdminCommandsLock = new ReentrantReadWriteLock();
 
   private final Collection<Command> commandBuffer = new ConcurrentLinkedQueue<>();
 
+  /**
+   * Creates and {@link #open() opens} a new {@link InjectionConnection} with the specified
+   * parameters.
+   *
+   * @param logFile the {@link #logFile} of the Minecraft installation
+   * @param worldDir the {@link #worldDir} of the Minecraft world
+   * @param identifier the unique {@link #identifier} for this connection in the Minecraft world
+   * @throws IOException if an I/O error occurs while {@link #open() opening} the connection
+   * @throws InterruptedException if the current thread is interrupted while waiting for Minecraft's
+   *         response
+   */
   public InjectionConnection(Path logFile, Path worldDir, String identifier)
       throws IOException, InterruptedException {
     this.logFile = checkNotNull(logFile, "logFile == null!");
@@ -86,19 +149,20 @@ public class InjectionConnection implements AutoCloseable {
     logger.info("Using structure '{}'", getStructureName(structureId.get()));
     executor = Executors.newSingleThreadScheduledExecutor();
     reader = new LogFileReader(logFile);
-    scheduleLogCheck();
+    schedulePeriodicLogCheck();
     Semaphore semaphore = new Semaphore(0);
     // Any successful command will do
     injectCommand("gamerule logAdminCommands true", e -> semaphore.release());
     flush();
     logger.info("Waiting for Minecraft's response");
     semaphore.acquire();
-    scheduleFlush();
+    schedulePeriodicFlush();
     logger.info("Successfully established {}", this);
   }
 
   @Override
   public void close() {
+    // TODO Adrodoc55 04.11.2017: Flush on close?
     if (isOpen()) {
       logger.info("Closing {}", this);
       if (isActive()) {
@@ -143,16 +207,16 @@ public class InjectionConnection implements AutoCloseable {
   }
 
   private void scheduleTasks() {
-    scheduleLogCheck();
-    scheduleFlush();
+    schedulePeriodicLogCheck();
+    schedulePeriodicFlush();
   }
 
-  private void scheduleLogCheck() {
+  private void schedulePeriodicLogCheck() {
     logCheckFuture =
         executor.scheduleAtFixedRate(this::checkLog, 0, logCheckPeriod, logCheckTimeUnit);
   }
 
-  private void scheduleFlush() {
+  private void schedulePeriodicFlush() {
     flushFuture = executor.scheduleAtFixedRate(() -> {
       try {
         flush();
@@ -164,16 +228,16 @@ public class InjectionConnection implements AutoCloseable {
   }
 
   private void cancelTasks() {
-    cancelLogCheck();
-    cancelFlush();
+    cancelPeriodicLogCheck();
+    cancelPeriodicFlush();
   }
 
-  private void cancelLogCheck() {
+  private void cancelPeriodicLogCheck() {
     logCheckFuture.cancel(false);
     logCheckFuture = null;
   }
 
-  private void cancelFlush() {
+  private void cancelPeriodicFlush() {
     flushFuture.cancel(false);
     flushFuture = null;
   }
@@ -193,6 +257,8 @@ public class InjectionConnection implements AutoCloseable {
   }
 
   /**
+   * Set the frequency at which the {@link #logFile} is checked for changes.
+   *
    * @param logCheckPeriod the new value for {@link #logCheckPeriod}
    * @param logCheckTimeUnit the new value for {@link #logCheckTimeUnit}
    */
@@ -200,8 +266,8 @@ public class InjectionConnection implements AutoCloseable {
     this.logCheckPeriod = logCheckPeriod;
     this.logCheckTimeUnit = checkNotNull(logCheckTimeUnit, "logCheckTimeUnit == null!");
     if (isActive()) {
-      cancelLogCheck();
-      scheduleLogCheck();
+      cancelPeriodicLogCheck();
+      schedulePeriodicLogCheck();
     }
   }
 
@@ -220,6 +286,8 @@ public class InjectionConnection implements AutoCloseable {
   }
 
   /**
+   * Set the frequency at which {@link #flush()} is invoked periodically.
+   *
    * @param flushPeriod the new value for {@link #flushPeriod}
    * @param flushTimeUnit the new value for {@link #flushTimeUnit}
    */
@@ -227,8 +295,8 @@ public class InjectionConnection implements AutoCloseable {
     this.flushPeriod = flushPeriod;
     this.flushTimeUnit = checkNotNull(flushTimeUnit, "flushTimeUnit == null!");
     if (isActive()) {
-      cancelFlush();
-      scheduleFlush();
+      cancelPeriodicFlush();
+      schedulePeriodicFlush();
     }
   }
 
@@ -292,39 +360,50 @@ public class InjectionConnection implements AutoCloseable {
   }
 
   public void flush() throws IOException {
+    // Prevent concurrent adding of commands that require admin command logging
+    // and synchronize the flush itself to prevent empty structures
+    logAdminCommandsLock.writeLock().lock();
     if (commandBuffer.isEmpty()) {
+      logAdminCommandsLock.writeLock().unlock();
       logger.trace("Skipping flush due to empty buffer");
       return;
     }
-    int structureId = this.structureId.getAndIncrement();
-
     Structure structure = new Structure(922, "Vanilla-Injection");
-    structure.setBackground(new SimpleBlockState("minecraft:air"));
-    structure
-        .addBlock(new StructureBlock(new Coordinate3I(), LOAD, getStructureName(structureId + 1)));
-
-    structure.addBlock(new SimpleBlock("minecraft:redstone_block", new Coordinate3I(0, 0, 2)));
-    structure.addBlock(new SimpleBlock("minecraft:activator_rail", new Coordinate3I(0, 1, 2)));
-
-    Coordinate3D minecartPos = new Coordinate3D(0.5, 1.0625, 2.5);
     boolean logAdminCommands = this.logAdminCommands;
-    synchronized (logAdminCommandsLock) {
-      this.logAdminCommands = false;
+    try {
       if (logAdminCommands) {
-        Command command = new Command("gamerule logAdminCommands true");
-        structure.addEntity(new CommandBlockMinecart(command, minecartPos));
+        structure.addEntity(newCommandBlockMinecart("gamerule logAdminCommands true"));
       }
       Iterators.consumingIterator(commandBuffer.iterator()).forEachRemaining(command -> {
-        structure.addEntity(new CommandBlockMinecart(command, minecartPos));
+        structure.addEntity(newCommandBlockMinecart(command));
       });
+      this.logAdminCommands = false;
+    } finally {
+      logAdminCommandsLock.writeLock().unlock();
     }
     if (logAdminCommands) {
-      Command command = new Command("gamerule logAdminCommands false");
-      structure.addEntity(new CommandBlockMinecart(command, minecartPos));
+      structure.addEntity(newCommandBlockMinecart("gamerule logAdminCommands false"));
     }
-    structure.addEntity(new CommandBlockMinecart(
-        new Command("kill @e[type=commandblock_minecart,dy=0]"), minecartPos));
+    structure.addEntity(newCommandBlockMinecart("kill @e[type=commandblock_minecart,dy=0]"));
+
+    int structureId = this.structureId.getAndIncrement();
+    structure
+        .addBlock(new StructureBlock(new Coordinate3I(), LOAD, getStructureName(structureId + 1)));
+    structure.addBlock(new SimpleBlock("minecraft:redstone_block", new Coordinate3I(0, 0, 2)));
+    structure.addBlock(new SimpleBlock("minecraft:activator_rail", new Coordinate3I(0, 1, 2)));
+    structure.setBackground(new SimpleBlockState("minecraft:air"));
+
     structure.writeTo(structureDir.resolve(getStructureName(structureId) + ".nbt").toFile());
+  }
+
+  private static final Coordinate3D MINECART_POS = new Coordinate3D(0.5, 1.0625, 2.5);
+
+  private static CommandBlockMinecart newCommandBlockMinecart(String command) {
+    return newCommandBlockMinecart(new Command(command));
+  }
+
+  private static CommandBlockMinecart newCommandBlockMinecart(Command command) {
+    return new CommandBlockMinecart(command, MINECART_POS);
   }
 
   private String getStructureName(int structureId) {
@@ -345,11 +424,14 @@ public class InjectionConnection implements AutoCloseable {
   }
 
   public void injectCommand(Command command, Consumer<SuccessEvent> listener) {
-    synchronized (logAdminCommandsLock) {
+    logAdminCommandsLock.readLock().lock();
+    try {
       logAdminCommands = true;
+      injectCommand(command);
+      addSuccessListener(command.getName(), false, listener);
+    } finally {
+      logAdminCommandsLock.readLock().unlock();
     }
-    injectCommand(command);
-    addSuccessListener(command.getName(), false, listener);
   }
 
   @Override
