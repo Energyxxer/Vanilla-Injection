0f4049f3b57cc8a1803c0c1675f4f2c217870892
Adrodoc55 <adrodoc55@googlemail.com>
2017-11-06 20:11:55 +0100

    Structure preserves block order to prevent hanging blocks like torches or rails from popping of

--- Vanilla Injection/src/de/adrodoc55/minecraft/structure/Structure.java
+++ Vanilla Injection/src/de/adrodoc55/minecraft/structure/Structure.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -57,7 +57,7 @@ public class Structure {
   /**
    * The {@link Block}s of this {@link Structure}.
    */
-  private final Map<Coordinate3I, Block> blocks = new HashMap<>();
+  private final Map<Coordinate3I, Block> blocks = new LinkedHashMap<>();
   /**
    * The {@link Entity entities} of this {@link Structure}.
    */
